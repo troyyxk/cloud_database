5efd7e1a2cc78029f344dc261d735113d823e2b7
Zhong Jefferson <jefferson.zhong@mail.utoronto.ca>
2021-01-25 23:22:37 -0800

    Connect data layer and client connection to server

--- src/app_kvServer/KVServer.java
+++ src/app_kvServer/KVServer.java
@@ -18,6 +18,7 @@ public class KVServer extends Thread implements IKVServer {
 	private int port;
 	private ServerSocket serverSocket;
 	private boolean running;
+	private DataAccessObject dao;
 
 
 	/**
@@ -26,11 +27,25 @@ public class KVServer extends Thread implements IKVServer {
 	 */
 	public KVServer(int port) {
 		this.port = port;
-//		this.strategy = "FIFO";
+		int cacheSize = 4096;
+		String strategy = "None";
+		this.dao = new DataAccessObject(cacheSize, strategy);
 	}
-//	publuc KVServer(int port, int cacheSize, String strategy) {
-//
-//	}
+
+	/**
+	 * Start KV Server at given port
+	 * @param port given port for storage server to operate
+	 * @param cacheSize specifies how many key-value pairs the server is allowed
+	 *           to keep in-memory
+	 * @param strategy specifies the cache replacement strategy in case the cache
+	 *           is full and there is a GET- or PUT-request on a key that is
+	 *           currently not contained in the cache. Options are "FIFO", "LRU",
+	 *           and "LFU".
+	 */
+	public KVServer(int port, int cacheSize, String strategy) {
+		this.port = port;
+		this.dao = new DataAccessObject(cacheSize, strategy);
+	}
 	
 	@Override
 	public int getPort(){
@@ -55,7 +70,7 @@ public class KVServer extends Thread implements IKVServer {
 				try {
 					Socket client = serverSocket.accept();
 					KVClientConnection connection =
-							new KVClientConnection(client);
+							new KVClientConnection(client, dao);
 					// TODO use thread pool
 					new Thread(connection).start();
 
