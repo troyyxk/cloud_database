8f7b5319b1fa61e9338b2aec22a07ad5f33d0a73
junxuan <atrocitytheme@gmail.com>
2021-01-27 02:15:44 -0500

    establish interface

--- src/shared/CommunicationSockMessageHandler.java
+++ src/shared/CommunicationSockMessageHandler.java
@@ -1,11 +1,22 @@
 package shared;
 
+import java.io.IOException;
 import java.net.Socket;
 
 // handle all possible socket message information and wrap them with certain protocolsssssss
-public class CommunicationSockMessageHandler {
+public class CommunicationSockMessageHandler implements ICommunicationSockHandler {
     private Socket socket;
     public CommunicationSockMessageHandler(Socket rawSocket) {
         this.socket = rawSocket;
+    }
+
+    @Override
+    public void sendMsg(String content) throws IOException {
+
+    }
+
+    @Override
+    public String getMsg() throws IOException {
+        return null;
     }
 }
