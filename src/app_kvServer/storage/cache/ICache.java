9fa5e1da5c83b6f62e9805b1e52b81503c32bf0e
zhongjef <jefferson.zhong@mail.utoronto.ca>
2021-01-28 03:15:38 -0800

    Adjust ICache for strategy pattern

--- src/app_kvServer/storage/cache/ICache.java
+++ src/app_kvServer/storage/cache/ICache.java
@@ -1,12 +1,41 @@
-package app_kvServer.storage;
+package app_kvServer.storage.cache;
 
+import app_kvServer.storage.IStorage;
+
+import java.util.Map;
+
+/**
+ * A cache is an object to temporarily store data.
+ */
 public interface ICache extends IStorage {
+
+    /**
+     * Strategy specifies the cache replacement strategy in case the cache
+     * is full and there is a GET- or PUT-request on a key that is
+     * currently not contained in the cache. Options are "FIFO", "LRU",
+     * and "LFU".
+     */
     public enum CacheStrategy {
         None,
         LRU,
         LFU,
         FIFO
     };
+
+    /**
+     * A Linked List implementation. Used for Cache implementation
+     * using FIFO or LRU strategy.
+     */
+    class LinkedNode {
+        protected String key;
+        protected String value;
+        protected LinkedNode prev, next;
+
+        LinkedNode(String key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
 
     /**
      * Get the cache strategy of the server
@@ -19,4 +48,11 @@ public interface ICache extends IStorage {
      * @return  cache size
      */
     public int getCacheSize();
+
+    /**
+     * Nominate a key for eviction to cache strategy
+     * @return return a candidate key for eviction
+     */
+    public String evict() throws Exception;
+
 }
