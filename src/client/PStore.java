a374494b2b29d0b5a41c3ec13257de182d84ebb7
troyyxk <39604949+troyyxk@users.noreply.github.com>
2021-01-28 13:14:07 -0500

    Add persistent storage

---
+++ src/client/PStore.java
@@ -0,0 +1,134 @@
+package client;
+
+import java.io.*;
+import java.util.*;
+
+import static java.lang.System.exit;
+import static java.lang.System.out;
+
+public class PStore{
+    private static String fileAddress = "./PStore.txt";
+    private static String delimiter = ",";
+
+    /**
+     * Check if key is in storage
+     * -1 for not found
+     * return is the byte location
+     * @param key
+     * @return byte location of the key
+     */
+    public static int contain(String key){
+        File store = new File(fileAddress);
+        // check if file does not exist
+        if (!(store.exists())) {
+            return -1;
+        }
+        int byteLocation = 0;
+        try {
+            FileReader fr = new FileReader(store);
+            BufferedReader br = new BufferedReader(fr);
+            String line = br.readLine();
+            while (line != null){
+                line = line.strip();
+                List<String> lines = Arrays.asList(line.split(delimiter));
+                // out.println(lines);
+                if (lines.get(0).equals(key)){
+                    return byteLocation;
+                }
+                // because 1 byte for every character
+                byteLocation += line.getBytes().length + 1;
+//                out.println(byteLocation);
+                line = br.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    /**
+     * Get the value for the key
+     * @param key
+     */
+    public static String getKV(String key){
+        File store = new File(fileAddress);
+        // check if file does not exist
+        if (!(store.exists())) {
+            exit(1);
+        }
+        try {
+            FileReader fr = new FileReader(store);
+            BufferedReader br = new BufferedReader(fr);
+            String line = br.readLine();
+            while (line != null){
+                line = line.strip();
+                List<String> lines = Arrays.asList(line.split(delimiter));
+                if (lines.get(0).equals(key)){
+                    return lines.get(1);
+                }
+                line = br.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        exit(1);
+        return "Not Found";
+    }
+
+    /**
+     * Put the key-value pair into storage
+     * If the storage file does not exist, create the file
+     * @param key
+     * @param value
+     */
+    public static void putKV(String key, String value) {
+        File store = new File(fileAddress);
+        // check if file does not exist
+        if (!(store.exists())) {
+            try {
+                store.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        // delete the og from file
+        int loc = contain(key);
+        if (loc >= 0){
+            out.println(loc);
+            try {
+                RandomAccessFile rf = new RandomAccessFile(fileAddress, "rw");
+                rf.seek(loc);
+                byte[] bytes = new byte[2];
+                rf.write(bytes);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // write to file
+        try {
+            FileWriter fw = new FileWriter(store, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(key + delimiter + value + '\n');
+            bw.close();
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Delete the persistent storage
+     */
+    public static void clear(){
+        File store = new File(fileAddress);
+        store.delete();
+    }
+
+}
