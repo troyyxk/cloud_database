1636a35f1202ade76513d068cf5fecb2ec2e2c13
troyyxk <39604949+troyyxk@users.noreply.github.com>
2021-01-29 02:43:08 -0500

    Add json pstore

--- src/app_kvServer/storage/persistence/PStore.java
+++ src/app_kvServer/storage/persistence/PStore.java
@@ -1,210 +1,86 @@
-package app_kvServer.storage.persistence;
+package client;
 
-import app_kvServer.storage.KeyNotFoundException;
-import org.apache.log4j.Logger;
 
 import java.io.*;
 import java.util.*;
+import org.json.*;
 
-
+import static java.lang.System.exit;
 import static java.lang.System.out;
 
+public class PStore implements IPersistence{
+    private String fileAddress = "./PStore.txt";
 
-/**
- * A persistence storage class,
- * Put takes O(1) and Get takes O(n)
- */
-public class PStore implements IPersistence {
-    private final String fileAddress;
-    private final String delimiter = ",";
-
-    private static Logger logger = Logger.getRootLogger();
-
-
-    public PStore(String fileAddress) {
-         this.fileAddress = fileAddress;
+    public boolean contains(String key){
+        JSONObject allPairs = readFile();
+        return allPairs.has(key);
     }
 
-    public boolean contains(String key) {
-        int loc = find_loc(key);
-        if (loc < 0) {
-            return false;
+    public String getKV(String key){
+        if (!contains(key)){
+            exit(1);
         }
-        return true;
+        JSONObject allPairs = readFile();
+        return allPairs.getString(key);
     }
 
-    /**
-     * Check if key is in storage
-     * -1 for not found
-     * return is the byte location
-     *
-     * @param key
-     * @return byte location of the key
-     */
-    public int find_loc(String key) {
-        File store;
-        try {
-            store = new File(fileAddress);
-            // check if file does not exist
-            if (!(store.exists())) {
-                return -1;
-            }
-        } catch (Exception ex) {
-            logger.error("Invalid file address");
-            return -1;
-        }
+    public void putKV(String key, String value){
+        JSONObject allPairs = readFile();
+        allPairs.put(key, value);
+        writeFile(allPairs);
+    }
 
-        int byteLocation = 0;
+    public void clear(){
+        File store = new File(fileAddress);
+        store.delete();
+    }
+
+    public JSONObject readFile(){
+        File store = new File(fileAddress);
+        // check if file does not exist
+        if (!(store.exists())) {
+            String str = "{}";
+            JSONObject obj = new JSONObject(str);
+            return obj;
+        }
+        String str = "{}";
         try {
             FileReader fr = new FileReader(store);
             BufferedReader br = new BufferedReader(fr);
-            String line = br.readLine();
-            while (line != null) {
-                line = line.strip();
-                List<String> lines = Arrays.asList(line.split(delimiter));
-                // out.println(lines);
-                if (lines.get(0).equals(key)) {
-                    return byteLocation;
-                }
-                // because 1 byte for every character
-                byteLocation += line.getBytes().length + 1;
-//                out.println(byteLocation);
-                line = br.readLine();
-            }
+            str = br.readLine();
+            br.close();
+            fr.close();
         } catch (FileNotFoundException e) {
-            logger.error("Cannot find file");
+            System.out.println("File not found");
         } catch (IOException e) {
-            logger.error("Cannot read from file");
+            System.out.println("Cannot read");
         }
-        return -1;
+        JSONObject allPairs = new JSONObject(str);
+        return allPairs;
+
     }
 
-    public String getKV(String key) throws KeyNotFoundException, IOException {
-        File store;
-        try {
-            store = new File(fileAddress);
-        } catch (Exception ex) {
-            logger.error("Invalid file address");
-            throw new IOException();
-        }
-
-        try {
-            FileReader fr = new FileReader(store);
-            BufferedReader br = new BufferedReader(fr);
-            String line = br.readLine();
-            while (line != null) {
-                line = line.strip();
-                List<String> lines = Arrays.asList(line.split(delimiter));
-                if (lines.get(0).equals(key)) {
-                    return lines.get(1);
-                }
-                line = br.readLine();
-            }
-            throw new KeyNotFoundException();
-        } catch (FileNotFoundException e) {
-            logger.error("Cannot find file");
-            throw new IOException();
-        } catch (IOException e) {
-            logger.error("Cannot read from file");
-            throw new IOException();
-        }
-    }
-
-    /**
-     * Put the key-value pair into storage
-     * If the storage file does not exist, create the file
-     * @param key
-     */
-    public void delete(String key) throws IOException, KeyNotFoundException {
-        // delete the og from file
-        int loc = find_loc(key);
-        if (loc < 0){
-            throw new KeyNotFoundException();
-        }
-        try {
-            RandomAccessFile rf = new RandomAccessFile(fileAddress, "rw");
-            rf.seek(loc);
-            byte[] bytes = new byte[2];
-            rf.write(bytes);
-        } catch (FileNotFoundException e) {
-            logger.error("Cannot find file");
-            throw new IOException();
-        } catch (IOException e) {
-            logger.error("Cannot read from file");
-            throw new IOException();
-        }
-    }
-
-    /**
-     * Put the key-value pair into storage
-     * If the storage file does not exist, create the file
-     * @param key
-     * @param value
-     */
-    public void putKV(String key, String value) throws IOException {
-
-        File store;
-        try {
-            store = new File(fileAddress);
-            // check if file does not exist
-            if (!(store.exists())) {
-                try {
-                    store.createNewFile();
-                } catch (IOException e) {
-                    logger.error("Cannot create new persistent file");
-                    throw new IOException();
-                }
-            }
-        } catch (Exception ex) {
-            logger.error("Invalid file address");
-            throw new IOException();
-        }
-
-        // delete the og from file
-        int loc = find_loc(key);
-        // key already in file, do update
-        if (loc >= 0){
+    public void writeFile(JSONObject pairs){
+        File store = new File(fileAddress);
+        // check if file does not exist
+        if (store.exists()) {
+            store.delete();
             try {
-                RandomAccessFile rf = new RandomAccessFile(fileAddress, "rw");
-                rf.seek(loc);
-                byte[] bytes = new byte[2];
-                rf.write(bytes);
-            } catch (FileNotFoundException e) {
-                logger.error("Cannot find file");
-                throw new IOException();
+                store.createNewFile();
             } catch (IOException e) {
-                logger.error("Cannot write to file");
-                throw new IOException();
+                System.out.println("Cannot create new file");
             }
-            return;
         }
-
         // write to file
+        String allPairs = pairs.toString();
         try {
             FileWriter fw = new FileWriter(store, true);
             BufferedWriter bw = new BufferedWriter(fw);
-            bw.write(key + delimiter + value + '\n');
+            bw.write(allPairs);
             bw.close();
             fw.close();
         } catch (IOException e) {
-            logger.error("Cannot write to file");
-            throw new IOException();
+            System.out.println("Cannot write file");
         }
     }
-
-    /**
-     * Delete the persistent storage
-     */
-    @Override
-    public void clearStorage() throws IOException{
-        try {
-            File store = new File(fileAddress);
-            store.delete();
-        } catch (Exception ex) {
-            logger.error("Cannot clear file");
-            throw new IOException();
-        }
-
-    }
-
 }
